2552944d50b2f2f6f4da48e53ec038a3b1d73aa6
Er. Abhinav Tiwari <TIWARIABHINAV273@GMAIL.COM>
2025-12-04 02:10:48 +0530

    Update que5.cpp

--- que5.cpp
+++ que5.cpp
@@ -1,18 +1,23 @@
 // Program to check whether a person is eligible to vote or not. // 
 
 #include <iostream>
+#include <string>
 using namespace std;
+
 int main() {
-    char person;
-    cout << "Enter a person";
+    string person;
+    cout << "Enter the person's name: ";
     cin >> person;
+
     int age;
-    cout << "Enter age of person";
+    cout << "Enter the person's age: ";
     cin >> age;
+
     if (age >= 18) {
         cout << person << " is eligible to vote." << endl;
     } else {
         cout << person << " is not eligible to vote." << endl;
     }
-return 0;
+
+    return 0;
 }
